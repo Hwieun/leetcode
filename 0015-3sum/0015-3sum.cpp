637ed493dedfb3a594cb600507eb091fdf8ff710
Hwieun <gk1vk@naver.com>
2022-11-17 00:10:39 +0900

    Time: 95 ms (90.68%), Space: 19.8 MB (95.36%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
         
         for(int i = 0; i < nums.size() - 2; i++) {
-            while(i > 0 && i < nums.size() - 2 && nums[i] == nums[i - 1]) i++;
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1, k = nums.size() - 1; j < k; j++) {
                 while(k > j && nums[j] + nums[k] + nums[i] > 0) k--;
                 if(k > j && nums[j] + nums[k] + nums[i] == 0) {
