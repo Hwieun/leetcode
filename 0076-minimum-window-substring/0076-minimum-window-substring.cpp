fb9bb8fb4cb1876f71b3403bd110b6dccbcdd373
Hwieun <gk1vk@naver.com>
2022-12-04 15:10:55 +0900

    Time: 68 ms (20.85%), Space: 8.1 MB (27.64%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         if(s.length() < t.length()) return "";
-        string ans = s;
-        bool b = false;
         unordered_map<char, int> um;
         for(char tt : t) um[tt]++;
 
-        // prev, cur 선언
-        int prev = 0, cur = 0, count = 0;
-        int start = 0, len = s.length();
-
-        // 현재 들고 있는 알파벳
-        unordered_map<char, int> temp;
-        while(cur < s.length()) {
-            if(um[s[cur]] <= 0) cur++;
+        // l: window 의 왼쪽 인덱스, r : window의 오른쪽 인덱스, cnt: window안에 일치하는 char 개수
+        // cnt == t.length() => 현재 window 내에 t의 char 가 전부 속해있음을 의미
+        int l = 0, r = 0, cnt = 0;
+        int astart = 0, alen = s.length();
+        unordered_map<char, int> window;
+        while(r < s.length()) {
+            // s[r]: t에 없는 char 인 경우
+            if(um[s[r]] == 0) {
+                r++;
+                continue;
+            }
+            // s[r]: t에 있는 char 인 경우
             else {
-                if(temp[s[cur]] < um[s[cur]]) count++;
-                temp[s[cur++]]++;
+                if(window[s[r]] < um[s[r]]) cnt++;
+                window[s[r++]]++;
             }
 
-            while(prev < cur && count == t.length()) {
-                b = true;
-                while(prev < cur && um[s[prev]] <= 0) prev++;
-                if(len > cur - prev) {
-                    start = prev;
-                    len = cur - prev;
+            // substring이 조건에 맞을 때. l을 다음으로 넘김
+            while(cnt == t.length()) {
+                while(l < r && um[s[l]] == 0) l++;
+                if(alen > r - l) {
+                    astart = l;
+                    alen = r - l;
                 }
-                if(temp[s[prev]] == um[s[prev]]) count--;
-                temp[s[prev++]]--;
+                if(window[s[l]] == um[s[l]]) cnt--;
+                window[s[l++]]--;
             }
         }
-        
-        return b ? s.substr(start, len) : "";
+
+        return l == 0 ? "" : s.substr(astart, alen);
     }
 };
